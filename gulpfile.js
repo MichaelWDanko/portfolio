4601a2b93289135637a308864808bd5235f96d63
Michael Danko <MichaelDanko@icloud.com>
2016-08-11 21:38:47 -0400

    Linked all files and added CSS normalize

--- gulpfile.js
+++ gulpfile.js
@@ -26,6 +26,11 @@ gulp.task('cname', function () {
         .pipe(gulp.dest('./public/'));
 });
 
+gulp.task('images', function () {
+    return gulp.src('./images/*')
+        .pipe(gulp.dest('./public/images/'));
+});
+
 gulp.task('watch', function () {
     gulp.watch('./*.html', ['html']);
     gulp.watch('./scss/*.scss', ['css']);
